733fe86fce81ff1281efd102ad45439eefd9b8af
Gabor Koncz1 <Gabor_Koncz1@epam.com>
2023-08-11 14:56:04 +0200

    created the db migrations and the postgres db

---
+++ BiblioTech.Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace BiblioTech.Infrastructure.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<LibraryDbContext>
+    {
+        public LibraryDbContext CreateDbContext( string[] args )
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory() + "\\..\\BiblioTech" )
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<LibraryDbContext>();
+            var connectionString = configuration.GetConnectionString( "DefaultConnection" );
+
+            builder.UseNpgsql(connectionString );
+
+            return new LibraryDbContext( builder.Options );
+        }
+    }
+}
